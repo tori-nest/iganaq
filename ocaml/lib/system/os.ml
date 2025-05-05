b33caae0d8137db783664e3c9720a00b9b4d3389
jutty <j@jutty.dev>
2025-05-05 10:55:52 -0300

    OCaml: Log contents of /etc/os_release for 'os' command (INS v0.2 B2.3)

--- ocaml/lib/system/os.ml
+++ ocaml/lib/system/os.ml
@@ -2,6 +2,8 @@
 
 let identify : string =
     let os_release = String.split_on_char '\n' (File.read "/etc/os-release") in
+    Utilities.Log.elog (String.concat "\n" os_release);
+
     let os_equals = List.find (String.starts_with ~prefix:"NAME=") os_release in
     match String.split_on_char '=' os_equals with
     | [ _; s ] ->
